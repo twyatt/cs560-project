e645ed84b615776c3b8a3f7e4e8863183d4ee13b
Travis Wyatt <travis.i.wyatt@gmail.com>
2015-04-19 18:38:11 -0700

    Added another console message.

--- src/edu/sdsu/cs560/project/Launcher.java
+++ src/edu/sdsu/cs560/project/Launcher.java
@@ -5,6 +5,7 @@ public class Launcher {
 	public static void main(String[] args) {
 		System.out.println("Hello world!");
 		System.out.println("CS560 sucks!");
+		System.out.println("testing again");
 	}
 	
 }
