d9848eb7894d64a72fcb6f9c97404c91b3ec6782
Travis Wyatt <travis.i.wyatt@gmail.com>
2015-04-29 23:27:56 -0700

    Updated solver to not use recursion (to fix stack overflows).

--- src/edu/sdsu/cs560/project/Launcher.java
+++ src/edu/sdsu/cs560/project/Launcher.java
@@ -1,11 +1,8 @@
 package edu.sdsu.cs560.project;
 
 import edu.sdsu.cs560.project.helpers.WoodenPuzzleBuilder;
-import edu.sdsu.cs560.project.models.Bitboard;
 import edu.sdsu.cs560.project.models.WoodenBlock;
 import edu.sdsu.cs560.project.models.WoodenPuzzle;
-
-import java.util.List;
 
 public class Launcher {
 
@@ -18,23 +15,13 @@ public class Launcher {
 				"C C D D I"
 		);
 
+		WoodenBlock solution = new WoodenBlock(puzzle.getBlockByName("J"));
+		solution.setValue(solution.shift(3, 0));
+
 		WoodenPuzzleSolver solver = new WoodenPuzzleSolver();
 
 		System.out.println(puzzle);
-		List<WoodenBlock> blocks = puzzle.getBlocks();
-		for (int i = 0; i < blocks.size(); i++) {
-			WoodenBlock block = blocks.get(i);
-			System.out.println("Block " + block.getName() + " @ " + i);
-		}
-		System.out.println(puzzle.getConfiguration());
-		solver.solve(puzzle);
-
-//		WoodenBlock F = puzzle.getBlockByName("F");
-//		F.setValue(F.shift(0, 1));
-//
-//		System.out.println(puzzle);
-//		System.out.println(puzzle.getConfiguration());
-//		solver.solve();
+		solver.solve(puzzle, solution);
 	}
 	
 }
--- src/edu/sdsu/cs560/project/WoodenPuzzleSolver.java
+++ src/edu/sdsu/cs560/project/WoodenPuzzleSolver.java
@@ -5,15 +5,28 @@ import edu.sdsu.cs560.project.models.WoodenBlock;
 import edu.sdsu.cs560.project.models.WoodenBlockMovement;
 import edu.sdsu.cs560.project.models.WoodenPuzzle;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class WoodenPuzzleSolver {
 
 	private final Map<Long, WoodenPuzzle> visited = new HashMap<>();
+	private final Queue<WoodenPuzzle> queue = new LinkedList<>();
 
-	public List<WoodenBlockMovement> solve(WoodenPuzzle p) {
+	private WoodenBlock solution;
+
+	public List<WoodenBlockMovement> solve(WoodenPuzzle p, WoodenBlock s) {
+		solution = s;
+		queue.add(p);
+		while (!queue.isEmpty()) {
+			solve();
+		}
+		return null;
+	}
+
+	public List<WoodenBlockMovement> solve() {
+		WoodenPuzzle p = queue.poll();
+		if (p == null) return null;
+
 		int occupied = Bitboard.combine(p.getBlocks()).getValue();
 
 		for (WoodenBlock b : p.getBlocks()) {
@@ -21,22 +34,22 @@ public class WoodenPuzzleSolver {
 
 			if (!p.isAtTop(b) && !WoodenBlock.overlaps(b.shift(0, -1), o)) {
 				// can move up
-				System.out.println(b.getName() + " can move up.");
+//				System.out.println(b.getName() + " can move up.");
 				move(p, b, 0, -1);
 			}
 			if (!p.isAtRight(b) && !WoodenBlock.overlaps(b.shift(1, 0), o)) {
 				// can move right
-				System.out.println(b.getName() + " can move right.");
+//				System.out.println(b.getName() + " can move right.");
 				move(p, b, 1, 0);
 			}
 			if (!p.isAtBottom(b) && !WoodenBlock.overlaps(b.shift(0, 1), o)) {
 				// can move down
-				System.out.println(b.getName() + " can move down.");
+//				System.out.println(b.getName() + " can move down.");
 				move(p, b, 0, 1);
 			}
 			if (!p.isAtLeft(b) && !WoodenBlock.overlaps(b.shift(-1, 0), o)) {
 				// can move left
-				System.out.println(b.getName() + " can move left.");
+//				System.out.println(b.getName() + " can move left.");
 				move(p, b, -1, 0);
 			}
 		}
@@ -48,18 +61,23 @@ public class WoodenPuzzleSolver {
 		WoodenBlock b = p.getBlockByName(block.getName());
 		b.setValue(b.shift(dx, dy));
 		long configuration = p.getConfiguration();
-		System.out.println(p);
+//		System.out.print(".");
 
-		// TODO check if the configuration matches the solution configuration
+//		System.out.println(p);
+		if (b.getName().equals(solution.getName()) && b.getValue() == solution.getValue()) {
+			System.out.println("Solution found!");
+			queue.clear();
+			return;
+		}
 
 		if (visited.containsKey(configuration)) {
 			// already tried this configuration
 			// TODO check if it took less steps this way than what is already stored, if so, replace existing entry in visited
-			System.out.println("already tried: " + configuration);
+//			System.out.println("already tried: " + configuration);
 		} else {
-			visited.put(p.getConfiguration(), p);
-			System.out.println("new configuration attempt: " + configuration);
-			solve(p);
+			visited.put(p.getConfiguration(), null);
+//			System.out.println("new configuration attempt: " + configuration);
+			queue.add(p);
 		}
 	}
 
